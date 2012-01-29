b7e339502454e3d99579038027c4d53a5f6efb7a
Chris Prather <chris@prather.org>
2012-01-29 00:14:24 -0500

    try String::Formatter per cjm's suggestion on irc

--- lib/Dist/Zilla/Plugin/GatherModuleDir/Template.pm
+++ lib/Dist/Zilla/Plugin/GatherModuleDir/Template.pm
@@ -4,43 +4,18 @@ use namespace::autoclean;
 
 # ABSTRACT: A FileGatherer for Module Directories
 
-extends qw(Dist::Zilla::Plugin::GatherDir);
+extends qw(Dist::Zilla::Plugin::GatherDir::Template);
 
-has replace_dirname => (
-    isa     => 'Str',
-    is      => 'ro',
-    default => 'Module'
-);
+use String::Formatter method_stringf => {
+    -as   => '_format_string',
+    codes => { N => sub { $_[0]->zilla->name }, },
+};
 
-sub _file_from_filename {
-    my ( $self, $filename ) = @_;
-
-    my $replace_dirname = $self->replace_dirname;
-    my $name            = $self->zilla->name;
-    ( my $newname = $filename ) =~ s/\Q$replace_dirname\E/$name/g;
-
-    my $template = do {
-        open my $fh, '<', $filename;
-        local $/;
-        <$fh>;
-    };
-
-    return Dist::Zilla::File::FromCode->new(
-        {   name => $newname,
-            mode => ( ( stat $filename )[2] & 0755 )
-                | 0200,   # kill world-writeability, make sure owner-writable.
-            code => sub {
-                my ($file_obj) = @_;
-                $self->fill_in_string(
-                    $template,
-                    {   dist   => \( $self->zilla ),
-                        plugin => \($self),
-                    },
-                );
-            },
-        }
-    );
-}
+around prefix => sub {
+    my ( $next, $self ) = @_;
+    my $prefix = $self->$next();
+    return _format_string( $prefix, $self );
+};
 
 __PACKAGE__->meta->make_immutable;
 1;
